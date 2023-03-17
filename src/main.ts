f11965272288787ed4197f7c5de57856509a0782
Coshido <coshido@gmail.com>
2023-03-17 13:01:59 +0100

    add: template and logic  to print the form data as li element

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Work } from "./Classes/work";
 import { Private } from "./Classes/private";
 import { HasFormatter } from "./Interfaces/HasFormatter";
+import { ListTemplate } from "./Classes/ListTemplate";
 
 // const form = document.querySelector('form')!;
 // if we select an html element directly we need the ! or ts assume the variable can be null.
@@ -13,6 +14,9 @@ const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
 const details = document.querySelector("#details") as HTMLInputElement;
 const amount = document.querySelector("#amount") as HTMLInputElement;
 
+const ul = document.querySelector("ul")!;
+const list = new ListTemplate(ul);
+
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
@@ -23,5 +27,5 @@ form.addEventListener("submit", (e: Event) => {
     doc = new Private(tofrom.value, details.value, amount.valueAsNumber);
   }
 
-  console.log(doc);
+  list.render(doc, type.value, "end");
 });
